f0f668a2addd105040ccd9c6f6c7f15d8d3724a7
Shashank-2021 <workingwithshashank@gmail.com>
2022-09-15 16:28:54 +0530

    Added top loading bar using top-loading-bar package

--- src/components/News.js
+++ src/components/News.js
@@ -29,16 +29,19 @@ export class News extends Component {
         document.title = `NewsMonkey - ${this.capitalizeFirstLetter(this.props.category)}`
     }
     async updateNews() {
-        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d4c156ccdae54272b2aa4750067958f6&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        this.props.setProgress(10);
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b154e61d1e954dae9dd440c6e79b28c0&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);
+        this.props.setProgress(30);
         let parsedData = await data.json();
-        console.log(parsedData);
+        this.props.setProgress(70);
         this.setState({
             articles: parsedData.articles,
             loading: false,
             totalResults: parsedData.totalResults
         })
+        this.props.setProgress(100);
     }
     async componentDidMount() {
         this.updateNews();
@@ -47,7 +50,7 @@ export class News extends Component {
         this.setState({
             page: this.state.page + 1
         });
-        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d4c156ccdae54272b2aa4750067958f6&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b154e61d1e954dae9dd440c6e79b28c0&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState({
@@ -60,7 +63,7 @@ export class News extends Component {
         return (
             <>
                 <h1 className="text-center" style={{ margin: "35px 0px" }} >NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
-                {this.state.loading && <Spinner/>}
+                {/* {this.state.loading && <Spinner/>} */}
                 <InfiniteScroll
                     dataLength={this.state.articles.length}
                     next={this.fetchMoreData}
