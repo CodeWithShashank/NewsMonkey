2a86da2d6b2d7b7ffcef31d363dd1f276d5e8cfe
Shashank-2021 <workingwithshashank@gmail.com>
2022-09-15 18:39:51 +0530

    Refactored all components to function based components except App.js

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
 import loadingBlack from './loadingBlack.gif'
 
-export class Spinner extends Component {
-  render() {
+const Spinner = ()=>{
     return (
       <div className="text-center">
         <img className='my-3' src={loadingBlack} alt= "Loading"/>
       </div>
     )
-  }
+  
 }
 
 export default Spinner
